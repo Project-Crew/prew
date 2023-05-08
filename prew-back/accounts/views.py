91b3b473bb3ec866e3e0ebc80944ac48c3c8e25c
iwri2089 <iwri2089@gmail.com>
2023-05-08 12:47:46 +0900

    feat: backend environment settings

---
+++ prew-back/accounts/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+def login(request):
+    pass
+
+def sign_up(request):
+    pass
+
+def delete_user(request):
+    pass
+
+def find_password(request):
+    pass
+
+
---
+++ prew-back/posts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def posts(request):
+    pass
+
+def comment(request):    # 비밀댓글 포함
+    pass
+
+def like(request):
+    pass
+
+def scrap(request):
+    pass
+
+def news_API(request):
+    pass
