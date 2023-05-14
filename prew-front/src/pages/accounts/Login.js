4a875e3a3b43cbf79c93129d84cc48a9058e04b0
조원주 <dnjswn9178@gmail.com>
2023-05-15 00:08:36 +0900

    feat: add Login scss

--- prew-front/src/pages/accounts/Login.js
+++ prew-front/src/pages/accounts/Login.js
@@ -3,6 +3,7 @@ import { InputText } from "primereact/inputtext";
 import { Password } from 'primereact/password';
 import { Button } from 'primereact/button';
 import { Checkbox } from "primereact/checkbox";
+import styles from "../../style/pages/accounts/Login.module.scss";
 
 const Login = () => {
     const [email, setEmail] = useState('');
@@ -10,30 +11,34 @@ const Login = () => {
     const [checked, setChecked] = useState(false);
     
     return (
-        <div>
-            Login
-            <h2>
-                <span>PREW</span>
+        <div className={styles.div}>
+            <h2 className={styles.logo}>
+                <span className={styles.logoColor}>PREW</span>
             </h2>
-            <form className='card justify-content-center'>
+            <form className='card'>
                 {/* primereact 디자인 적용 input field */}
-                <div className="p-float-label p-inputtext-lg">
-                    <InputText id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <div className={`p-float-label ${styles.form}`}>
+                    <InputText id="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
                     <label htmlFor="email">PREW ID (아이디 또는 이메일)</label>
                 </div>
-                <div className="p-float-label p-inputtext-lg">
-                    <Password id="password" value={password} onChange={(e) => setPassword(e.target.value)} feedback={false} />
+                <div className={`p-float-label ${styles.form}`}>
+                    <Password id="password" value={password} onChange={(e) => setPassword(e.target.value)} feedback={false}/>
                     <label htmlFor="password">비밀번호 (문자, 특수문자, 숫자를 포함해주세요)</label>
                 </div>
 
-                <label className="p-float-label p-inputtext-lg">
+                <label className={`p-float-label ${styles.form}`}>
                     <Checkbox onChange={e => setChecked(e.checked)} checked={checked}></Checkbox> 로그인 상태 유지
                 </label>
         
-                <Button label="PREW 로그인" type="submit" />
+                <Button label="PREW 로그인" type="submit" className={`${styles.form} mt-0`}/>
             </form>
-            <Button label="회원가입 하기" link />
-            <Button label="비밀번호 찾기" link />
+
+            <div>
+                <Button label="회원가입 하기" link />
+                <Button label="비밀번호 찾기" link />
+            </div>
+
+            <p>SNS로 로그인 하기</p>
         </div>
     );
 };
