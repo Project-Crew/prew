4324a06627b7545bb6403bb1871b77e2d419dd77
nexusin <nexusin@naver.com>
2023-05-15 02:16:44 +0900

    feat: signup, login, logout, password reset

--- prew-back/accounts/urls.py
+++ prew-back/accounts/urls.py
@@ -1,9 +1,27 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
-    path('login/',views.login),
-    path('sign-up/',views.sign_up),
-    path('delete-user/',views.delete_user),
-    path('find-password/',views.find_password)
+    # index test page
+    path('', views.index, name='index'),
+
+    path('login/', views.login, name='account_login'),
+    path('logout/', views.login, name='account_logout'),
+    path('signup/', views.sign_up, name='account_signup'),
+
+    # 비밀번호 관련
+    # 비밀번호 찾기 페이지 (비밀번호 재설정 링크를 받을 이메일을 입력하는 페이지)
+    path('password/reset/', views.find_password, name='account_reset_password'),
+    # 비밀벉호 재설정 이메일 전송 완료 페이지
+    path('password/reset/done/', views.find_password, name='account_reset_password_done'),
+    # 비밀번호 재설정 페이지
+    path('password/reset/key/', views.find_password, name='account_reset_password_from_key'),
+    # 비밀번호 재설정 완료 페이지
+    path('password/reset/key/done/', views.find_password, name='account_reset_password_from_key_done'),
+
+    
+    path('unregister/', views.delete_user, name='unregister'),
+    path('email-confirmation-done/', TemplateView.as_view(template_name="accounts/email_confirmation.html"), name='account_email_confirmation'),
 ]
--- prew-back/accounts/views.py
+++ prew-back/accounts/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.urls import reverse
+from allauth.account.views import PasswordChangeView
+from allauth.socialaccount.forms import DisconnectForm
 
 # Create your views here.
+
+# test index
+def index(request):
+    return render(request, 'account/index.html')
+
 def login(request):
     pass
 
@@ -13,4 +21,21 @@ def delete_user(request):
 def find_password(request):
     pass
 
+# 비밀번호를 변경하면 index page로 redirect
+class CustomPasswordChangeView(PasswordChangeView):
+    def get_success_url(self):  
+        return reverse("index")
 
+
+def socialaccount_connections(request):
+
+    if request.method == 'POST':
+        form = DisconnectForm(request.POST, initial={'account': account})
+        if form.is_valid():
+            form.save()
+            return redirect('socialaccount_connections')
+    else:
+        form = DisconnectForm(initial={'account': account})
+
+
+    return render(request, 'socialaccount_connections.html', {'form': form})
