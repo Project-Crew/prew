e50e0684c038557632def682d25aa21dd036dba7
장은희 <6810779s@naver.com>
2023-05-15 15:43:05 +0900

    feat: wrong word

--- prew-back/userprofile/models.py
+++ prew-back/userprofile/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 class Posts(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=30)  # 임의
-    content=models.TextField()  # 임의
-    likes=models.ManyToManyField(User,related_name='post_likes')
+    content = models.TextField()  # 임의
+    likes = models.ManyToManyField(User,related_name='post_likes')
 
 class Comment(models.Model):
-    conetne=models.TextField() # 임의
+    content=models.TextField() # 임의
     likes=models.ManyToManyField(User,related_name='post_likes')
 
 # Create your models here.
@@ -17,8 +17,8 @@ class Profile(models.Model):
     user_name=models.CharField(max_length=20)
     email = models.CharField(max_length=255)
     nickname = models.CharField(max_length=10)
-    profileImg = models.ImageField(upload_to="imgs/")
+    profile_img = models.ImageField(upload_to="imgs/")
     my_comments = models.ManyToManyField(Comment,related_name='comment')
-    myPosts = models.ManyToManyField(Posts, related_name='my_post', blank=True)
-    myScraps = models.ManyToManyField(Posts,related_name='post_scrap',blank=True)
+    my_posts = models.ManyToManyField(Posts, related_name='my_post', blank=True)
+    my_scraps = models.ManyToManyField(Posts,related_name='post_scrap',blank=True)
     
