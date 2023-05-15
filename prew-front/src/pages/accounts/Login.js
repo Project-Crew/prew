78b0d1c48e24e5cfdd938a72798fb04b4e500f83
조원주 <dnjswn9178@gmail.com>
2023-05-15 17:17:20 +0900

    feat:login구현중

--- prew-front/src/pages/accounts/Login.js
+++ prew-front/src/pages/accounts/Login.js
@@ -1,21 +1,78 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+// import { useDispatch } from "react-redux";
 import { InputText } from "primereact/inputtext";
 import { Password } from 'primereact/password';
 import { Button } from 'primereact/button';
 import { Checkbox } from "primereact/checkbox";
 import styles from "../../style/pages/accounts/Login.module.scss";
+// import axios from 'axios'
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [checked, setChecked] = useState(false);
     
+    const [loading, setLoading] = useState(false);
+    const [alertMsg, setMsg] = useState('');
+    
+    // const dispatch = useDispatch();
+    // let user = userSelector((state) => {return state.user});
+    
+    useEffect(() => {
+        if (alertMsg) {
+            setTimeout(() => {
+                setMsg('');
+                setLoading(false);
+            }, 1500)
+        }
+    }, [alertMsg, loading])
+    /*
+    // id 와 pw 입력되었는지 확인
+    const LoginFunc = (e) => {
+        e.preventDefault();
+        if (!email) {
+            return alert("ID를 입력하세요");
+        }
+        else if (!password) {
+            return alert("Password를 입력하세요");
+        }
+        else {
+            let body = {
+                email,
+                password
+            };
+
+            axios.post("Endpoint", body)
+            .then((res) => {
+                switch (res.data.code) {
+                    case 200:
+                        dispatch(loginUser(res.data.userInfo));
+                        break;
+                    case 400:
+                        setMsg("ID, Password가 비어있습니다");
+                        break;
+                    case 401:
+                        setMsg("존재하지 않는 ID 입니다");
+                        break;
+                    case 402:
+                        setMsg("Password가 틀립니다");
+                        break;
+                    default:
+                        break;
+                }
+            })
+        }
+    }
+    */
+
     return (
         <div className={styles.div}>
             <h2 className={styles.logo}>
                 <span className={styles.logoColor}>PREW</span>
             </h2>
+            {/* onSubmit={LoginFunc} */}
             <form className='card'>
+                <p>{alertMsg}</p>
                 {/* primereact 디자인 적용 input field */}
                 <div className={`p-float-label ${styles.form}`}>
                     <InputText id="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
@@ -30,7 +87,7 @@ const Login = () => {
                     <Checkbox onChange={e => setChecked(e.checked)} checked={checked}></Checkbox> 로그인 상태 유지
                 </label>
         
-                <Button label="PREW 로그인" type="submit" className={`${styles.form} mt-0`}/>
+                <Button label="PREW 로그인" type="submit" disabled={loading} className={`${styles.form} mt-0`}/>
             </form>
 
             <div>
