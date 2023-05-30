8605346ea6e208facc35e118fd74b3206332c5e9
gus991121 <gus991121@naver.com>
2023-05-31 00:22:54 +0900

    feat: post, comment, like, scrap

--- prew-back/posts/serializers.py
+++ prew-back/posts/serializers.py
@@ -1,12 +1,31 @@
 from rest_framework import serializers
 from .models import Comment, Post
 
-class CommentSerializer(serializers.ModelSerializer):
+class CommentSerializer(serializers.ModelSerializer):    
     class Meta:
         model = Comment
         fields='__all__'
+        read_only_fields = ('post', 'user',)
+
 
 class PostSerializer(serializers.ModelSerializer):
+    post_comment = CommentSerializer(many=True, read_only=True)
+
     class Meta:
         model = Post
         fields='__all__'
+        read_only_fields = ('user', )
+
+
+    # 댓글 표시할 때 댓글 내용 제한
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        user = self.context['request'].user
+
+        # 게시글 작성자 또는 비밀댓글을 작성한 사용자인 경우 댓글 내용 표시
+        if instance.user == user or any(comment['is_secret'] and comment['user'] == user for comment in data['post_comment']):
+            return data
+
+        # 댓글 내용이 비밀이거나 권한이 없는 경우 댓글 개수만 표시
+        data['post_comment'] = '비밀 댓글입니다.'
+        return data
--- prew-back/posts/views.py
+++ prew-back/posts/views.py
@@ -1,56 +1,117 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import Post
-from .forms import PostForm
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from django.shortcuts import get_object_or_404, get_list_or_404
+
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
 # Create your views here.
 
 
-
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
 def posts(request):
     if request.method == 'GET':
-        posts = Post.object.all()
-        context = {
-            'posts': posts
-        }
-        return render(request, '', context)
+        posts = get_list_or_404(Post)
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
     
     elif request.method == 'POST':
-        if request.user.is_authenticated:
-            form = PostForm(request.POST)
-            if form.is_valid():
-                post = form.save()
-                return redirect('post_detail', post_pk = post.pk)
-        return redirect('accounts:login')
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
+@api_view(['GET', 'DELETE', 'PUT'])
 def post_detail(request, post_pk):
     post = get_object_or_404(Post, pk=post_pk)
 
-    if request.method == 'POST':
-        if request.user.is_authenticated and request.user == post.user:
-            form = PostForm(request.POST, instance=post)
-            if form.is_valid():
-                form.save()
-                return redirect('post_detail', post_pk=post.pk)
+    if request.method == 'GET':
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
     
-    else:
-        form = PostForm(instance=post)
+    elif request.method == 'DELETE':
+        if post.user == request.user:
+            post.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+    elif request.method == 'PUT':
+        serializer = PostSerializer(post, data=request.data)
+        if post.user == request.user:
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def comment(request, post_pk):    # 비밀댓글 포함 -> serializers에서 처리
+    post = get_object_or_404(Post, pk=post_pk)
+
+    serializer = CommentSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save(user=request.user, post=post)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticated])
+def comment_detail(request, comment_pk):
+    comment = get_object_or_404(Comment, pk=comment_pk)
+    if request.method == 'GET':
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
     
-    context = {
-        'post': post,
-        'form': form,
-    }
-    return render(request, '', context)
+    elif request.method == 'PUT':
+        if comment.user == request.user:
+            serializer = CommentSerializer(comment, data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(serializer.data)
+    
+    elif request.method == 'DELETE':
+        if comment.user == request.user:
+            comment.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-def comment(request):    # 비밀댓글 포함
-    pass
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def like_post(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+    user = request.user
+    if post.likes.filter(pk=user.pk).exists():
+        post.likes.remove(user)
+        return Response(status=status.HTTP_200_OK)
+    post.likes.add(user)
+    return Response(status=status.HTTP_200_OK)
 
-def like(request):
-    pass
 
-def scrap(request):
-    pass
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def scrap_post(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+    user = request.user
+    if post.scraps.filter(pk=user.pk).exists():
+        post.scraps.remove(user)
+        return Response(status=status.HTTP_200_OK)
+    post.scraps.add(user)
+    return Response(status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def like_comment(request, comment_pk):
+    comment = get_object_or_404(Comment, pk=comment_pk)
+    user = request.user
+    if comment.likes.filter(pk=user.pk).exists():
+        comment.likes.remove(user)
+        return Response(status=status.HTTP_200_OK)
+    comment.likes.add(user)
+    return Response(status=status.HTTP_200_OK)
 
 def news_API(request):
     pass
