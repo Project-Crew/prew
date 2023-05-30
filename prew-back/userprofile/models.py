007afd2cc39d90a2a57b774862f161ca5069fb55
eunhee <6810779s@naver.com>
2023-05-30 10:25:51 +0900

    feat: profile api쪽 구현중

--- prew-back/userprofile/models.py
+++ prew-back/userprofile/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Posts(models.Model):
+class Posts(models.Model):    # posts는 추후에 posts
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=30)  # 임의
     content=models.TextField()  # 임의
@@ -13,12 +13,12 @@ class Comment(models.Model):
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
-    user_name=models.CharField(max_length=20)
-    email = models.CharField(max_length=255)
-    nickname = models.CharField(max_length=10)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="profile")
+    # user_name=models.CharField(max_length=20)   # User model에서 제공
+    # email = models.CharField(max_length=255)   # User model에서 제공
+    # nickname = models.CharField(max_length=10)    # User model에서 제공
     profileImg = models.ImageField(upload_to="imgs/")
     my_comments = models.ManyToManyField(Comment,related_name='comment')
-    myPosts = models.ManyToManyField(Posts, related_name='my_post', blank=True)
-    myScraps = models.ManyToManyField(Posts,related_name='post_scrap',blank=True)
+    # myPosts = models.ManyToManyField(Posts, related_name='my_post', blank=True)
+    # myScraps = models.ManyToManyField(Posts,related_name='post_scrap',blank=True)
     
