62dd0abdf53a95788b91310533226cdc1491af90
eunhee <6810779s@naver.com>
2023-05-31 22:43:27 +0900

    feat: profile my posts 화면

--- prew-front/src/components/NoPosts.js
+++ prew-front/src/components/NoPosts.js
@@ -1,11 +1,17 @@
-import React from 'react';
-
+import React from "react";
+import { BsPencilSquare } from "react-icons/bs";
 const NoPosts = () => {
-    return (
-        <div>
-            NoPosts
-        </div>
-    );
+  return (
+    <div className="flex flex-column justify-content-center text-center">
+      <div className="pt-7 pb-3">
+        <BsPencilSquare className="text-7xl" />
+      </div>
+      <h3 className="pb-2">게시물이 존재하지 않습니다.</h3>
+      <a href="#" className="text-primary no-underline">
+        게시물 쓰러 가기
+      </a>
+    </div>
+  );
 };
 
 export default NoPosts;
--- prew-front/src/pages/profile/MyPosts.js
+++ prew-front/src/pages/profile/MyPosts.js
@@ -1,11 +1,68 @@
-import React from 'react';
-
+import React, { useEffect, useState } from "react";
+import styles from "../../style/pages/profile/MyPosts.module.scss";
+import classNames from "classnames/bind";
+import { Tooltip } from "primereact/tooltip";
+import NoPosts from "../../components/NoPosts";
+const cx = classNames.bind(styles);
 const MyPosts = () => {
-    return (
-        <div>
-            MyPosts
-        </div>
-    );
+  const [myPosts, setMyPosts] = useState([]);
+  const dummyPosts = [
+    {
+      id: 1,
+      title: "내가쓴 게시물 제목이다.1",
+      content: "게시물 내용",
+      date: "2023.05.04",
+      author: "myPost",
+    },
+    {
+      id: 2,
+      title: "내가쓴 게시물 제목이다.2",
+      content: "게시물 내용",
+      date: "2023.05.04",
+      author: "eun",
+    },
+    {
+      id: 3,
+      title: "내가쓴 게시물 제목이다.3",
+      content: "게시물 내용",
+      date: "2023.05.04",
+      author: "h",
+    },
+    {
+      id: 4,
+      title: `내가쓴 게시물 제목이다.
+      내가쓴 게시물 제목이다.
+        내가쓴 게시물 제목이다.
+        내가쓴 게시물 제목이다.내가쓴 게시물 제목이다.`,
+      content: "게시물 내용",
+      date: "2023.05.04",
+      author: "myPost",
+    },
+  ];
+  useEffect(() => {
+    const posts = dummyPosts.filter((v) => v.author === "myPost");
+    setMyPosts(posts);
+  }, []);
+
+  return (
+    <ul className={styles.container}>
+      {myPosts.length > 0 ? (
+        myPosts.map((v) => (
+          <li className="flex target pt-4">
+            <Tooltip target={`.title${v.id}`} mouseTrack>
+              {v.title}
+            </Tooltip>
+            <span className={cx("flex-1", `title${v.id}`, styles.title)}>
+              {v.title.length > 50 ? v.title.slice(0, 50) + "..." : v.title}
+            </span>
+            <span className={styles.date}>{v.date}</span>
+          </li>
+        ))
+      ) : (
+        <NoPosts />
+      )}
+    </ul>
+  );
 };
 
 export default MyPosts;
