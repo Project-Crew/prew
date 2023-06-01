7bcdce0171d10ebe3a90258aefa9bdb973df2a50
eunhee <6810779s@naver.com>
2023-06-01 12:36:47 +0900

    feat:myscraps 화면

--- prew-front/src/pages/profile/MyScraps.js
+++ prew-front/src/pages/profile/MyScraps.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+
 import NoPosts from "../../components/NoPosts";
 import classNames from "classnames/bind";
 import styles from "../../style/pages/profile/MyScraps.module.scss";
 
+import { Checkbox } from "primereact/checkbox";
+
 const cx = classNames.bind(styles);
 const MyScraps = () => {
+  const [selected, setSelected] = useState([]);
+  const onSelectedChange = (e) => {
+    let _selectedCategories = [...selected];
+
+    if (e.checked) {
+      _selectedCategories.push(e.value);
+    } else
+      _selectedCategories = _selectedCategories.filter(
+        (category) => category.id !== e.value.id
+      );
+
+    setSelected(_selectedCategories);
+  };
   const dummyPosts = [
     {
       id: 1,
@@ -63,24 +80,43 @@ const MyScraps = () => {
     },
   ];
   return (
-    <ul>
+    <ul className={styles.container}>
       {dummyPosts.length > 0 ? (
         dummyPosts.map((v) => (
-          <li className={cx()}>
-            <div>
+          <li className={cx("flex py-2")}>
+            <div key={v.id} className="flex align-items-center pr-4">
+              {/* <input type="checkbox" /> */}
+              {/* <Controller
+                name="checkbox"
+                control={control}
+                render={({ field }) => <Checkbox {...field} />}
+              /> */}
+              <Checkbox
+                inputId={v.id}
+                name="category"
+                value={v}
+                onChange={onSelectedChange}
+                checked={selected.some((item) => item.id === v.id)}
+              />
+            </div>
+            <div className={cx("flex-1 line-height-4")}>
               <p>{v.topic}</p>
-              <p>
+              <p className={cx("font-semibold")}>
                 {v.title.length > 50 ? v.title.slice(0, 50) + "..." : v.title}
               </p>
               <p>
-                <span>모집 입원 {v.total_personnel}</span>
-                <span>{v.place}</span>
+                <span className="pr-2">모집 입원 {v.total_personnel}명</span>
+                <span className="pr-2">{v.place}</span>
                 <span>{v.skill.length > 0 && v.skill[0]}</span>
               </p>
             </div>
-            <div>
-              <p>~{v.expired_date}</p>
-              <p>
+            <div
+              className={cx(
+                "flex flex-column justify-content-center text-right text-500"
+              )}
+            >
+              <p className={cx("py-1")}>~{v.expired_date}</p>
+              <p className={cx("py-1")}>
                 모집된 인원 {v.now_personnel}/{v.total_personnel}
               </p>
             </div>
